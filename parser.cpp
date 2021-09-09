e7eb9547ba1f73fa18aa181b754969593de96c3e
oasiskharkov <kasystems@yandex.ru>
2021-09-09 19:01:23 +0300

    fix some bugs

--- parser.cpp
+++ parser.cpp
@@ -28,37 +28,41 @@ void Parser::parse()
       {
          std::wstring weatherTmpl = L"<div class=\"weather__content\"><a aria-label=\"";
          int pos = wstr.find(weatherTmpl);
-         if (pos == std::wstring::npos)
+         if (pos != std::wstring::npos)
          {
-            continue;
+            pos += weatherTmpl.length();
+            std::wstring wfc = wstr.substr(pos, wstr.find('\"', pos) - pos);
+            pos = wfc.find(',');
+            m_weather.setTemperature(wfc.substr(0, pos));
+            m_weather.setFallout(wfc.substr(pos + 2, wfc.length() - (pos + 2)));
+            weatherFound = true;
          }
-         pos += weatherTmpl.length();
-         
-         std::wstring wfc = wstr.substr(pos, wstr.find('\"', pos) - pos);
-         pos = wfc.find(',');
-         m_weather.setTemperature(wfc.substr(0, pos));
-         m_weather.setFallout(wfc.substr(pos + 2, wfc.length() - (pos + 2)));
-         weatherFound = true;
       }
       if (!usdFound)
       {
          std::wstring curTmpl = L"\"inline-stocks__value_inner\">";
          std::wstring usdTmpl = L"USD";
          int pos = wstr.find(usdTmpl);
-         pos = wstr.find(curTmpl, pos + usdTmpl.length());
-         pos += curTmpl.length();
-         m_usd.setCurrency(wstr.substr(pos, wstr.find('<', pos) - pos));
-         usdFound = true;
+         if (pos != std::wstring::npos)
+         {
+            pos = wstr.find(curTmpl, pos + usdTmpl.length());
+            pos += curTmpl.length();
+            m_usd.setCurrency(wstr.substr(pos, wstr.find('<', pos) - pos));
+            usdFound = true;
+         }
       }
       if (!eurFound)
       {
          std::wstring curTmpl = L"\"inline-stocks__value_inner\">";
          std::wstring eurTmpl = L"EUR";
          int pos = wstr.find(eurTmpl);
-         pos = wstr.find(curTmpl, pos + eurTmpl.length());
-         pos += curTmpl.length();
-         m_eur.setCurrency(wstr.substr(pos, wstr.find('<', pos) - pos));
-         eurFound = true;
+         if (pos != std::wstring::npos)
+         {
+            pos = wstr.find(curTmpl, pos + eurTmpl.length());
+            pos += curTmpl.length();
+            m_eur.setCurrency(wstr.substr(pos, wstr.find('<', pos) - pos));
+            eurFound = true;
+         }
       }
       
       if (weatherFound and usdFound and eurFound)
